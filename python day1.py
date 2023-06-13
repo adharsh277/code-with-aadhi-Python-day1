f0642d7cb5e18b55439be6903477db2e34468f81
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-06-13 12:52:14 +0530

    Create python day1.py

---
+++ python day1.py
@@ -0,0 +1,11 @@
+# cjgshvchscbvh
+# sduysvb
+# The  people of the situation of the regional cast of the democracy of the country
+print("hello world")
+print(7)
+print(8+63)
+print(9//3)
+print("qknha")
+print("when the sun rise in the east your beauty makes me to fall down from the heaven that nbody everr have realized it is that one")
+print("hery iam a good boi \n and this guy is also so good")
+print("hey lionel messi have described himself as the \"worlds best player\" ever seen in the history of the world that has been seen outside of the world ")
