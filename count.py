2b24931e5aa7c2b7b6268b92fdb40651a3028da6
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-06-19 16:30:24 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+a="harry !!!!!harry!!!!messi"
+print(a.count("r"))
+print(a.count("a"))
+print(a.count("!"))
+b="welcome to the shinobhi world pal!!!"
+print(b.endswith("pal"))
+print(b.endswith("!"))
+b="he's name is dan. he is an man"
+print(b.find("is"))
